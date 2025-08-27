bea66cc97a6c56435117372b1e3540ae2d48c7ec
Ying-art-prog <narutobentancor@gmail.com>
2025-08-27 09:30:24 -0300

    multimedios

---
+++ index.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector('.menu__dropDown');
+const menuList = document.querySelector('.menu__list');
+const menu = document.querySelector('.nav__menu');
+
+menuToggle.addEventListener('click', () => {
+    menuList.classList.toggle('menu__list--visible');
+    menu.classList.toggle('nav__menu--expanded');
+    menu.style.width = menu.style.width === '50%' ? '15%' : '50%';
+    menu.style.height = menu.style.height === '30rem' ? '30rem' : '30rem';
+    menu.style.transition = 'width 0.3s ease, height 0.3s ease';
+    menuList.style.display = menuList.style.display === 'flex' ? 'none' : 'flex';
+});
