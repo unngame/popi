1585474d88756ad39ee92ccb2642df735c3dada6
06Scop <lnoe27199@gmail.com>
2025-08-30 00:26:04 -0300

    Add language selection and enhance content for Mitre and La Nación pages
    - Introduced a language selection fieldset in index.html and mitre.html for English, Spanish, and Portuguese.
    - Updated JavaScript to adjust menu dimensions for better responsiveness.
    - Enhanced mitre.html with additional articles about Bartolomé Mitre's contributions and legacy.
    - Created new language-specific HTML files for English and Portuguese versions of La Nación and Mitre content.
    - Updated CSS styles for improved layout and responsiveness across different screen sizes.

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const menu = document.querySelector('.nav__menu');
 menuToggle.addEventListener('click', () => {
     menuList.classList.toggle('menu__list--visible');
     menu.classList.toggle('nav__menu--expanded');
-    menu.style.width = menu.style.width === '50%' ? '15%' : '50%';
-    menu.style.height = menu.style.height === '40rem' ? '3rem' : '40rem';
+    menu.style.width = menu.style.width === '40%' ? '12%' : '40%';
+    menu.style.height = menu.style.height === '38rem' ? '3rem' : '38rem';
     menu.style.transition = 'width 0.3s ease, height 0.3s ease';
     menuList.style.display = menuList.style.display === 'flex' ? 'none' : 'flex';
 });
